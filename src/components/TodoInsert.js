5ebe01661d6afca238d712022c64b4f9392348cb
benzamin0 <crazyybogus@gmail.com>
2020-01-25 01:15:45 +0900

    first commit

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,35 @@
+import React, {useState, useCallback} from 'react';
+import {MdAdd} from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({onInsert}) => {
+
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e=>{
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback (
+        e => {
+            onInsert(value);
+            setValue('');
+
+            //prevent refresh from submit event
+            e.preventDefault();
+        }, [onInsert, value],
+    );
+    return (
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="Put your Todo into here"
+                value = {value}
+                onChange = {onChange}
+            />
+            <button type="submit">
+                <MdAdd/>
+            </button>
+        </form>
+    );
+};
+
+export default TodoInsert;
